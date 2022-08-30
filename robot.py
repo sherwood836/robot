63213fb21454f43e38a4a65ae8db7dbe7d515fed
Tommy Di <tdisessa@gmail.com>
2022-08-29 20:35:04 -0500

    Added type hints

--- robot.py
+++ robot.py
@@ -1,13 +1,13 @@
 cardinal_list = ['N', 'E', 'S', 'W']
 cardinal_pos_map = {'N':0, 'E':1, 'S':2, 'W':3}
 
-def turn_right(x, y, direction):
+def turn_right(x: int, y: int, direction: str) -> tuple:
     return x, y, cardinal_list[(cardinal_pos_map[direction] + 1) % 4]
 
-def turn_left(x, y, direction):
+def turn_left(x: int, y: int, direction: str) -> tuple:
     return x, y, cardinal_list[(cardinal_pos_map[direction] - 1) % 4]
 
-def move_1(x, y, direction, max_x, max_y):
+def move_1(x: int, y: int, direction: str, max_x: int, max_y: int) -> tuple:
     if 'N' == direction:
         y = (y + 1) if y != max_y else y
     elif 'S' == direction:
@@ -19,7 +19,7 @@ def move_1(x, y, direction, max_x, max_y):
 
     return x, y, direction
 
-def make_move(x, y, direction, move, max_x, max_y):
+def make_move(x: int, y: int, direction: str, move: str, max_x: int, max_y: int) -> tuple:
     if 'M' == move:
         x, y, direction = move_1(x, y, direction, max_x, max_y)
     elif 'L' == move:
@@ -31,13 +31,13 @@ def make_move(x, y, direction, move, max_x, max_y):
 
     return x, y, direction
 
-def take_trip(x, y, direction, move_list, max_x, max_y):
+def take_trip(x: int, y: int, direction: str, move_list: str, max_x: int, max_y: int) -> tuple:
         for move in move_list:
            x, y, direction = make_move(int(x), int(y), direction, move, max_x, max_y) 
 
         return x, y, direction
 
-def error_check_start(start_position, max_x, max_y):
+def error_check_start(start_position: str, max_x: int, max_y: int) -> tuple:
     if not start_position:
         return None
 
@@ -56,7 +56,7 @@ def error_check_start(start_position, max_x, max_y):
 
     return x, y, direction
 
-def error_check_coordinate(coordinate, max_coordinate, error_message):
+def error_check_coordinate(coordinate: str, max_coordinate: int, error_message: str) -> int:
     try:
         coordinate = int(coordinate)
     except:
@@ -70,7 +70,7 @@ def error_check_coordinate(coordinate, max_coordinate, error_message):
     
     return coordinate
 
-def error_check_max(max_position):
+def error_check_max(max_position: str) -> tuple:
     max_position_list = max_position.split(' ')
 
     if len(max_position_list) == 2:    
@@ -83,7 +83,7 @@ def error_check_max(max_position):
 
     return x, y
 
-def program():
+def program() -> list:
     output_print_list = []
 
     max_x, max_y = error_check_max(input())
